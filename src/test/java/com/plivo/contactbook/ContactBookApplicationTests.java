0807e50cb3e52d7801890f8a8edb6fac1165d2ec
Yogender Yadav <yogenderyadav@mbk-tech-461-ub.mobikwik.com>
2020-04-18 15:16:14 +0530

    initializing the app

---
+++ src/test/java/com/plivo/contactbook/ContactBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plivo.contactbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
